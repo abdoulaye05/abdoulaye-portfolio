b57d1e6f86a5b0d04241c2c119bc5685ba39f08e
abdoulaye05 <abdoulaye.diallo02@edu.univ-fcomte.fr>
2024-12-22 21:50:04 +0100

    Déploiement des fichiers générés

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/abdoulaye-portfolio/', // Chemin relatif pour GitHub Pages
+  base: '/abdoulaye-portfolio/', // Chemin correspondant au nom du dépôt sur GitHub Pages
+  build: {
+    rollupOptions: {
+      input: './index.html', // Spécifie le fichier d'entrée principal
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+      external: [
+        '/abdoulaye-portfolio/assets/index-CtmMOE-6.js', // Ajoute les fichiers manquants ici
+        '/abdoulaye-portfolio/assets/index-QklJJWgL.css',
+      ],
+    },
+  },
 });
